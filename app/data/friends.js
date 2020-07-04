bb3d24b6a9771824344df032446964b78c2e20d9
Somin Kang <somin.kang.91@gmail.com>
2020-07-04 13:58:54 -0700

    updates

---
+++ app/data/friends.js
@@ -0,0 +1,51 @@
+var friendList = [
+    {
+          name: 'Jenny',
+          profilePic: '',
+          scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+          ]
+      },
+    {
+          name: 'Timothy',
+          profilePic: '',
+          scores: [
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5",
+            "5"
+          ]
+      },
+      {
+          name: 'Psy',
+          profilePic: '',
+          scores: [
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1",
+            "1"
+          ]
+      }
+  ]
+  module.exports = friendList;
