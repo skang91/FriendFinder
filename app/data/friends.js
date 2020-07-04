a22a89bbb57bb3cad8d341bb89f81005577efd99
Somin Kang <somin.kang.91@gmail.com>
2020-07-04 16:39:10 -0700

    update list

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 var friends = [
 	{
     name: "Jenny",
-    photo: "",
+    photo: "https://images.app.goo.gl/dKCdS5KTWiqCLmY68",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
   },
   {
     name: "Timthoy",
-    photo: "",
+    photo: "https://images.app.goo.gl/9MpXjt5mY2qpAUjJA",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
   },
   {
     name: "Psy",
-    photo: "",
+    photo: "https://images.app.goo.gl/Gyfb4ApkKr8FVjFf9",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
   }
   ];
