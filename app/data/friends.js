4bdb8cda26dfb8ddfb5423098fcbb22877c7d29b
Somin Kang <somin.kang.91@gmail.com>
2020-07-04 16:57:54 -0700

    update photos

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,17 +1,17 @@
 var friends = [
 	{
-    name: "Jenny",
-    photo: "https://images.app.goo.gl/dKCdS5KTWiqCLmY68",
+    name: "Jennie",
+    photo: "https://i.pinimg.com/736x/b0/d5/11/b0d5115db2fe549ff57aad329d4b3260.jpg",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
   },
   {
     name: "Timthoy",
-    photo: "https://images.app.goo.gl/9MpXjt5mY2qpAUjJA",
+    photo: "https://lh3.googleusercontent.com/74Gm4D-e5TFIaU1BkymwXiQkvODMig_0CFd0zsX8efq93PJbyGZPZlzxsiZnrJQPmy3-pwERACFysuywYSVv5lOp",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
   },
   {
     name: "Psy",
-    photo: "https://images.app.goo.gl/Gyfb4ApkKr8FVjFf9",
+    photo: "https://q4x7t3q8.map2.ssl.hwcdn.net/assets/images/members/psy-m1.jpg",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
   }
   ];
