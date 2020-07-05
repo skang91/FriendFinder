637aee509a236103d723cb1c443216ed69eb013d
Somin Kang <somin.kang.91@gmail.com>
2020-07-04 19:46:24 -0700

    update friendlist

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,26 +1,26 @@
 var friends = [
 	{
-    name: "Jennie",
-    photo: "https://kidolprofile.weebly.com/uploads/9/8/5/8/98586920/published/blackpink-jennie-kim.jpeg?1489865822",
+    name: "Jennie Kim",
+    photo: "https://files.combyne.com/a0cbdafbf6d33abbbdb9e5c516ab989e_image.jpg",
 		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
   },
   {
-    name: "Timthoy",
-    photo: "https://lh3.googleusercontent.com/74Gm4D-e5TFIaU1BkymwXiQkvODMig_0CFd0zsX8efq93PJbyGZPZlzxsiZnrJQPmy3-pwERACFysuywYSVv5lOp",
+    name: "Henry Lau",
+    photo: "https://l-hit.com/img_id/299431.jpg",
 		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
   },
   {
-    name: "Psy",
-    photo: "https://q4x7t3q8.map2.ssl.hwcdn.net/assets/images/members/psy-m1.jpg",
+    name: "Tzuyu Chou",
+    photo: "https://kpopping.com/uploads/documents/kpics_related/xFF34279E-CDDA-4365-AA38-53AD054F23C0.jpeg.crop.ffffff.png.pagespeed.ic.kpd7sg54WA.jpg",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
   },
 	{
-		name: "Lisa",
+		name: "Lalisa Manoban",
 		photo: "https://i.pinimg.com/564x/19/9d/be/199dbe053c92fc7ff8250ddd15291d89.jpg",
 		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
 	},
 	{
-		name: "Rose",
+		name: "Roseanne Park",
 		photo: "https://i.pinimg.com/564x/f2/5c/54/f25c548f20e7b65c01e10ab2e48ad856.jpg",
 		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
   }
