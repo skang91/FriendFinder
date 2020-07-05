66def4745a1783218e7441aaaac603ce105e92f3
Somin Kang <somin.kang.91@gmail.com>
2020-07-04 17:41:42 -0700

    updates friend list

--- app/data/friends.js
+++ app/data/friends.js
@@ -13,6 +13,16 @@ var friends = [
     name: "Psy",
     photo: "https://q4x7t3q8.map2.ssl.hwcdn.net/assets/images/members/psy-m1.jpg",
 		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+  },
+	{
+		name: "Lisa",
+		photo: "https://i.pinimg.com/564x/19/9d/be/199dbe053c92fc7ff8250ddd15291d89.jpg",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Rose",
+		photo: "https://i.pinimg.com/564x/f2/5c/54/f25c548f20e7b65c01e10ab2e48ad856.jpg",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
   }
   ];
   module.exports = friends;
